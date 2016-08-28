c9f1bdd1f24f2edd4952e0890b71afb82edff927
Jack <Jack@mail.ru>
2016-08-28 22:34:19 +0400

    tutorial 6: using ViewSets, binding ViewSets to URLs explicitly

--- snippets/views.py
+++ snippets/views.py
@@ -4,18 +4,11 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
-
-class SnippetHighlight(generics.GenericAPIView):
-	queryset = Snippet.objects.all()
-	renderer_classes = (renderers.StaticHTMLRenderer,)
-
-	def get(self, request, *args, **kwargs):
-		snippet = self.get_object()
-		return Response(snippet.highlighted)
+from rest_framework import viewsets
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -24,24 +17,21 @@ def api_root(request, format=None):
 		'snippets': reverse('snippet-list', request=request, format=format),
 	})
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+	
+	@detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+	def highlight(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
+
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-
-class UserDetail(generics.RetrieveAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-
 
